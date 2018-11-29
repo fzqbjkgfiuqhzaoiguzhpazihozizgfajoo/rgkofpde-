9b50c235c4b982bdfa3a0482277b9bcfc6604c46
fzqbjkgfiuqhzaoiguzhpazihozizgfajoo <45123656+fzqbjkgfiuqhzaoiguzhpazihozizgfajoo@users.noreply.github.com>
2018-11-29 23:41:03 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,25 +1,15 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const developers = ['463698464845594628','514641715651543041'];
+const developers = ['463698464845594628','515786788078485515'];
 
 client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
     if (!developers.includes(message.author.id)) return;
 
-if (message.content.startsWith('Rwt')) {
-client.user.setActivity(argresult, {type:'WATCHING'});
-  client.user.setStatus("dnd")
-    message.channel.send(` ** Done. :white_check_mark: ** `)
-} else 
 if (message.content.startsWith('Rls')) {
 client.user.setActivity(argresult , {type:'LISTENING'});
   client.user.setStatus("idle")
     message.channel.send(` ** Done. :white_check_mark: ** `)
-} else 
-if (message.content.startsWith('Rst')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/iitxbro");
-  client.user.setStatus("idle")
-   message.channel.send(` ** Done. :white_check_mark: ** `)
 } else 
 if (message.content.startsWith('Rpl')) {
 client.user.setActivity(argresult , {type:'PLAYING'});
@@ -29,9 +19,12 @@ client.user.setActivity(argresult , {type:'PLAYING'});
 }
   client.on('message', message => {//iiHouSSaM#9494
         if (!developers.includes(message.author.id)) return;//iiHouSSaM#9494
-  if (message.content === '20') {//iiHouSSaM#9494
-  
-  let channel = client.channels.get('514641715651543041');//iiHouSSaM#9494
+  if (message.content === '999') {//iiHouSSaM#9494
+
+//if (!channel) return generale.join() .then(connection => message.channel.send(` \` ** Done.:white_check_mark:  ** `));  
+
+  let channel = client.channels.get('515786788078485515');//iiHouSSaM#9494
+
 
   channel.join()//iiHouSSaM#9494
   .then(connection => console.log(`????`))//iiHouSSaM#9494
