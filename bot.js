eaeba4ca04eac67ed56650f4c4c5a030983cd114
fzqbjkgfiuqhzaoiguzhpazihozizgfajoo <45123656+fzqbjkgfiuqhzaoiguzhpazihozizgfajoo@users.noreply.github.com>
2018-12-14 22:13:08 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const developers = ['463698464845594628','437836985659293707'];
+const developers = ['463698464845594628','523208758797533184'];
 
 client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
@@ -8,22 +8,22 @@ client.on('message', message => {
 
 if (message.content.startsWith('Rls')) {
 client.user.setActivity(argresult , {type:'LISTENING'});
-  client.user.setStatus("idle")
+  client.user.setStatus("dnd")
     message.channel.send(` ** Done. :white_check_mark: ** `)
 } else 
 if (message.content.startsWith('Rpl')) {
 client.user.setActivity(argresult , {type:'PLAYING'});
-  client.user.setStatus("idle")
+  client.user.setStatus("dnd")
     message.channel.send(` ** Done. :white_check_mark: ** `)
 
 }
   client.on('message', message => {//iiHouSSaM#9494
         if (!developers.includes(message.author.id)) return;//iiHouSSaM#9494
-  if (message.content === 'لا اله الا الله') {//iiHouSSaM#9494
+  if (message.content === 'ol') {//iiHouSSaM#9494
 
 //if (!channel) return generale.join() .then(connection => message.channel.send(` \` ** Done.:white_check_mark:  ** `));  
 
-  let channel = client.channels.get('437836985659293707');//iiHouSSaM#9494
+  let channel = client.channels.get('523208758797533184');//iiHouSSaM#9494
 
 
   channel.join()//iiHouSSaM#9494
