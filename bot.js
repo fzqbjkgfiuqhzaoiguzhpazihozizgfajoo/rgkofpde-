0cd8be66694810c46c4094d4b197216c8ecd8602
fzqbjkgfiuqhzaoiguzhpazihozizgfajoo <45123656+fzqbjkgfiuqhzaoiguzhpazihozizgfajoo@users.noreply.github.com>
2018-12-15 17:14:29 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const developers = ['463698464845594628','520562326676570112'];
+const developers = ['463698464845594628','523287498630365184'];
 
 client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
@@ -17,7 +17,7 @@ client.user.setActivity(argresult , {type:'LISTENING'});
     message.channel.send(` ** Done. :white_check_mark: ** `)
 } else 
 if (message.content.startsWith('Rst')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/iitxbro");
+  client.user.setGame(argresult, "https://www.twitch.tv/ii7m0dy");
   client.user.setStatus("dnd")
    message.channel.send(` ** Done. :white_check_mark: ** `)
 } else 
@@ -27,16 +27,16 @@ client.user.setActivity(argresult , {type:'PLAYING'});
     message.channel.send(` ** Done. :white_check_mark: ** `)
 
 }
-  client.on('message', message => {//iiHouSSaM#9494
-        if (!developers.includes(message.author.id)) return;//iiHouSSaM#9494
-  if (message.content === '18') {//iiHouSSaM#9494
+  client.on('message', message => {//-  AG              Ráááááz  , $!#8811 
+        if (!developers.includes(message.author.id)) return;//-  AG              Ráááááz  , $!#8811 
+  if (message.content === '88') {//-  AG              Ráááááz  , $!#8811 
 
-  let channel = client.channels.get('520562326676570112');//iiHouSSaM#9494
+  let channel = client.channels.get('520562326676570112');//-  AG              Ráááááz  , $!#8811 
 
 
-  channel.join()//iiHouSSaM#9494
-  .then(connection => console.log(`????`))//iiHouSSaM#9494
-  .catch(console.error);//iiHouSSaM#9494
+  channel.join()//-  AG              Ráááááz  , $!#8811 
+  .then(connection => console.log(`????`))//-  AG              Ráááááz  , $!#8811 
+  .catch(console.error);//-  AG              Ráááááz  , $!#8811 
   }
 });
           
