4aaeccdc49bb9d7cf413ce3b85b2c2cd1b7228d5
fzqbjkgfiuqhzaoiguzhpazihozizgfajoo <45123656+fzqbjkgfiuqhzaoiguzhpazihozizgfajoo@users.noreply.github.com>
2018-12-16 17:22:50 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
     if (!developers.includes(message.author.id)) return;
 
-if (message.content.startsWith('Rwt')) {
+if (message.content.startsWith('RRwt')) {
 client.user.setActivity(argresult, {type:'WATCHING'});
   client.user.setStatus("dnd")
     message.channel.send(` ** Done. :white_check_mark: ** `)
@@ -17,7 +17,7 @@ client.user.setActivity(argresult , {type:'LISTENING'});
     message.channel.send(` ** Done. :white_check_mark: ** `)
 } else 
 if (message.content.startsWith('Rst')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/ii7m0dy");
+  client.user.setGame(argresult, "https://www.twitch.tv/iitxbro");
   client.user.setStatus("dnd")
    message.channel.send(` ** Done. :white_check_mark: ** `)
 } else 
@@ -27,21 +27,20 @@ client.user.setActivity(argresult , {type:'PLAYING'});
     message.channel.send(` ** Done. :white_check_mark: ** `)
 
 }
-  client.on('message', message => {//-  AG              Ráááááz  , $!#8811 
-        if (!developers.includes(message.author.id)) return;//-  AG              Ráááááz  , $!#8811 
-  if (message.content === '88') {//-  AG              Ráááááz  , $!#8811 
+  client.on('message', message => {//iiHouSSaM#9494
+        if (!developers.includes(message.author.id)) return;//iiHouSSaM#9494
+  if (message.content === '963') {//iiHouSSaM#9494
 
-  let channel = client.channels.get('523764005592891392');//-  AG              Ráááááz  , $!#8811 
+  let channel = client.channels.get('523764005592891392');//iiHouSSaM#9494
 
 
-  channel.join()//-  AG              Ráááááz  , $!#8811 
-  .then(connection => console.log(`????`))//-  AG              Ráááááz  , $!#8811 
-  .catch(console.error);//-  AG              Ráááááz  , $!#8811 
+  channel.join()//iiHouSSaM#9494
+  .then(connection => console.log(`????`))//iiHouSSaM#9494
+  .catch(console.error);//iiHouSSaM#9494
   }
 });
           
 });
-
 
 client.login(process.env.BOT_TOKEN);
 
